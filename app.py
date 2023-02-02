72b0477ea803a8fc92bbdbeb9dce1ea2b48161a4
Matthew Allinson <mrallinson@gmail.com>
2023-02-02 12:48:17 +0000

    makes flask run with aircraft data

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import planeTracker as pt
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-	return "<p>Hello, World!</p>"
+def index():
+	planes = pt.get_planes()
+	data = pt.show_frame(planes['flyovers']).to_html()
+
+	return render_template("index.html", data=data)
